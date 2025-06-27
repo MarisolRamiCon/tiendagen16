ae5267fbd0bc321b51f5ff523319a15abfa8fb34
Gabino <gabino.hernandez.sab@gmail.com>
2025-06-26 21:26:33 -0600

    Update Invenatrio Entity

--- tiendagen16/src/main/java/ms/tiendagen16/entity/Inventario.java
+++ tiendagen16/src/main/java/ms/tiendagen16/entity/Inventario.java
@@ -3,10 +3,16 @@ package ms.tiendagen16.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "inventario")
+@Table(name = "INVENTARIO")
 public class Inventario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "PRODUCTO")
+    private Integer producto;
+
+    @Column(name = "CANTIDAD_STOCK")
+    private Integer cantidadStock;
 
 }
